1ff72c351f51414d85b963a4884322841290e42f
Varun Pai M D <91287778+varunpai314@users.noreply.github.com>
2025-09-29 01:31:59 +0530

    Removed DB URL from Public Access

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-DATABASE_URL = "postgresql+asyncpg://medinote_user:StrongSecurePassword123@localhost/medinote_db"
-
-engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+engine = create_async_engine(os.getenv("DATABASE_URL"), echo=True, future=True)
 AsyncSessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 Base = declarative_base()
